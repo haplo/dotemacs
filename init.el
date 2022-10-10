a87160b2f36d7dc35279e473f40121c96cb08328
Fidel Ramos <f@fidelramos.net>
2022-10-10 08:59:31 +0100

    Use use-package for savehist

--- init.el
+++ init.el
@@ -181,14 +181,15 @@
         uniquify-ignore-buffers-re "^\\*"))
 
 ;; savehist keeps track of some history
-(require 'savehist)
-(setq savehist-additional-variables
-      ;; search entries
-      '(search-ring regexp-search-ring)
-      ;; save every minute
-      savehist-autosave-interval 60
-      ;; keep the home clean
-      savehist-file (expand-file-name "savehist" my-savefile-dir))
+(use-package savehist
+  :config
+  (setq savehist-additional-variables
+        ;; search entries
+        '(search-ring regexp-search-ring)
+        ;; save every minute
+        savehist-autosave-interval 60
+        ;; keep the home clean
+        savehist-file (expand-file-name "savehist" my-savefile-dir)))
 
 ;; save recent files
 (require 'recentf)
