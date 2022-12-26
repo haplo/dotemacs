47417b182e7e55c761772e618469a1baa1dcbfcd
Fidel Ramos <f@fidelramos.net>
2022-12-26 16:33:25 +0100

    Use use-package for ansi-color

--- init.el
+++ init.el
@@ -475,11 +475,13 @@
 
 ;; Colorize output of Compilation Mode, see
 ;; http://stackoverflow.com/a/3072831/355252
-(require 'ansi-color)
-(defun colorize-compilation-buffer ()
-  (let ((inhibit-read-only t))
-    (ansi-color-apply-on-region (point-min) (point-max))))
-(add-hook 'compilation-filter-hook #'colorize-compilation-buffer)
+(use-package ansi-color
+  :hook
+  (compilation-filter . colorize-compilation-buffer)
+  :preface
+  (defun colorize-compilation-buffer ()
+    (let ((inhibit-read-only t))
+      (ansi-color-apply-on-region (point-min) (point-max)))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;; vertico, consult, marginalia, embark, orderless ;;;
