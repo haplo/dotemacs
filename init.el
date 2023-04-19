104881e46e44b807e98a7106ddd4746026eb84e0
Fidel Ramos <f@fidelramos.net>
2023-04-19 09:40:32 +0100

    ^ for directory up in dired/dirvish

--- init.el
+++ init.el
@@ -500,24 +500,25 @@
   :demand t
   :bind
   (("C-x C-j" . dired-jump)
+   :map dired-mode-map
+   ("^"   . 'dired-up-directory)
    :map dirvish-mode-map  ; Dirvish inherits `dired-mode-map'
-   ("a"   . dirvish-quick-access)
-   ("f"   . dirvish-file-info-menu)
-   ("y"   . dirvish-yank-menu)
-   ("N"   . dirvish-narrow)
-   ("^"   . dirvish-history-last)
-   ("h"   . dirvish-history-jump) ; remapped `describe-mode'
-   ("s"   . dirvish-quicksort)    ; remapped `dired-sort-toggle-or-edit'
-   ("v"   . dirvish-vc-menu)      ; remapped `dired-view-file'
-   ("TAB" . dirvish-subtree-toggle)
-   ("M-f" . dirvish-history-go-forward)
-   ("M-b" . dirvish-history-go-backward)
-   ("M-l" . dirvish-ls-switches-menu)
-   ("M-m" . dirvish-mark-menu)
-   ("M-t" . dirvish-layout-toggle)
-   ("M-s" . dirvish-setup-menu)
-   ("M-e" . dirvish-emerge-menu)
-   ("M-j" . dirvish-fd-jump)
+   ("a"   . 'dirvish-quick-access)
+   ("f"   . 'dirvish-file-info-menu)
+   ("y"   . 'dirvish-yank-menu)
+   ("N"   . 'dirvish-narrow)
+   ("h"   . 'dirvish-history-jump) ; remapped `describe-mode'
+   ("s"   . 'dirvish-quicksort)    ; remapped `dired-sort-toggle-or-edit'
+   ("v"   . 'dirvish-vc-menu)      ; remapped `dired-view-file'
+   ("TAB" . 'dirvish-subtree-toggle)
+   ("M-f" . 'dirvish-history-go-forward)
+   ("M-b" . 'dirvish-history-go-backward)
+   ("M-l" . 'dirvish-ls-switches-menu)
+   ("M-m" . 'dirvish-mark-menu)
+   ("M-t" . 'dirvish-layout-toggle)
+   ("M-s" . 'dirvish-setup-menu)
+   ("M-e" . 'dirvish-emerge-menu)
+   ("M-j" . 'dirvish-fd-jump)
    )
   :init
   (dirvish-override-dired-mode)
